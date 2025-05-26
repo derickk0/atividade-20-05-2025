57fab61838b8a9528a9c4071a825bc0340cfbeb3
Erick Breno <159458075+derickk0@users.noreply.github.com>
2025-05-26 10:33:23 -0300

    Delete api directory

--- api/src/main/java/com/example/aula/exception/NumeroCamisaJaCadastrado.java
+++
@@ -1,7 +0,0 @@
-package com.example.aula.exception;
-
-public class NumeroCamisaJaCadastrado extends RuntimeException {
-  public NumeroCamisaJaCadastrado(String message) {
-    super(message);
-  }
-}
